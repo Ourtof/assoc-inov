87f893b3b0c8be393e016852c5c218000d514570
Yann <yann.eysseric@egs.school>
2022-07-19 15:45:59 +0200

    1er commit

---
+++ include/scripts/header.php
@@ -0,0 +1,23 @@
+<?php
+function entete() {
+
+return "<header>
+        <div class=logo>
+        </div>
+        <div id=bandeau>
+            <h1>ASSOC'INOV</h1>
+            <h3>L'association où il fait bon vivre</h3>
+        </div>
+        
+        <nav>
+            <ul>
+                <li>Accueil</li>
+                <li>Adhérents</li>
+                <li>Activités</li>
+                <li>Animateurs</li>
+                <li>Actualités</li>
+            </ul>
+        </nav>
+    </header>";
+}
+?>
