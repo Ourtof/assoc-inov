01e7eef5df91f7f1294b84048e79b5a96760c5c5
Yann <yann.eysseric@egs.school>
2022-07-20 19:17:49 +0200

    modif actualite

--- actualite.php
+++ actualite.php
@@ -16,8 +16,15 @@
 <body>
 
     <?php
-   include_once("include/scripts/header.php");
-   include_once("include/scripts/footer.php");
-    ?>
+   include_once('include/scripts/header.php');
+
+   $mois = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septebre', 'Octobre', 'Novembre', 'Décembre');
+   $mois_num = array('Janvier'=>1, 'Février'=>2, 'Mars'=>3, 'Avril'=>4, 'Mai'=>5, 'Juin'=>6, 'Juillet'=>7, 'Août'=>8, 'septembre'=>9, 'Octobre'=>10, 'Novembre'=>11, 'Décembre'=>12);
+   $saison = array('été', 'hiver', 'automne', 'printemps');
+
+   echo "Le mois d'$mois[7] est en été.";
+
+   include_once('include/scripts/footer.php');
+   ?>
 
 </body>
